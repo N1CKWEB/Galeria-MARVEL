9ed954f33a9fbe43d09d7b7caab6c122a6f9624b
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-02-18 22:18:03 -0300

    Siguiendo con los estilos en css y empezando a manipular el HTML con DOM js

--- Dinamica/app.js
+++ Dinamica/app.js
@@ -1,2 +1,16 @@
+const h1=document.createElement("h1");
+
+h1.textContent="Holaa";
+
+h1.innerHTML="Hola";
 
 
+document.querySelector(".scrooling-buttons-left-active").addEventListener("click", ()=> {
+    window.location.href = 'index.html';
+});
+
+document.querySelector(".scrooling-buttons-right-active").addEventListener("click", ()=> {
+
+    window.location.href="index2.html";
+
+});
