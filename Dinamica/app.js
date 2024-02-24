511f23507a21a3e877c801796c6f6451f698b379
N1CKWEB <91564857+N1CKWEB@users.noreply.github.com>
2024-02-24 01:43:44 -0300

    Proyecto finalizado, le falta agregar un poco más de dinamica y interactividad con js

---
+++ Dinamica/app.js
@@ -0,0 +1,8 @@
+
+document.querySelector('.arrowRight').addEventListener('click', function() {
+    window.location.href = 'vengadores.html';
+});
+
+document.querySelector('.arrowLeft').addEventListener('click', function() {
+    window.location.href = 'villanos.html';
+});
